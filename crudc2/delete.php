1e4e72049f9371ce89b68bdafc206caf1a01a412
ebad-uddin <ebad@gmail.com>
2023-09-04 13:48:21 +0500

    product addition functionality added

--- crudc2/delete.php
+++ crudc2/delete.php
@@ -11,6 +11,6 @@ $delete = "UPDATE `@stds` SET status = '0'  where id = '$userid'";
 $result = mysqli_query($connect, $delete);
 if(!$result){die("query failed");
 }
-header('location:http://localhost:82/classc2/crud/allusers.php');
+header('location:allusers.php');
 
 ?>
